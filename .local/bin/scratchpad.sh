815353d98f7fe1f457665f4dc7e45c3f81ee1319
Juan Ramirez <juanc.rampar@gmail.com>
2023-08-03 16:02:47 -0500

    Edit sxhkdrc

--- .local/bin/scratchpad.sh
+++ .local/bin/scratchpad.sh
@@ -2,4 +2,4 @@
 
 #tdrop -a -w 70% -h 60% -y 20% -x 15%  --class scratch alacritty --class=scratch #--hold -e khal calendar
 
-tdrop -a -w 70% -h 60% -y 20% -x 15%  --class scratch alacritty --class=scratch --hold -e calcurse
+tdrop -a -w 70% -h 60% -y 20% -x 15%  --class scratch alacritty --class=scratch 
