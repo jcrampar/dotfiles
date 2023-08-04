70caf3b4ae66783921abfffacd836998da4406a4
Juan Ramirez <juanc.rampar@gmail.com>
2023-08-03 20:10:16 -0500

    Setting monocle layout

---
+++ .config/bspwm/scripts/change-window-monocle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+declare -A dtoc=([north]=prev [south]=next [west]=prev [east]=next)
+case "$(jq -r '.layout' < <(bspc query -T -d))" in
+    tiled) t="${2}.local";;
+    monocle) t="${dtoc[$2]}.local.window.!hidden";;
+    *) exit 1;;
+esac
+bspc node "$1" "$t"
