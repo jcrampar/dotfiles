0c4f6df0d3086fc299ccaf9c7a3837d66aec8e25
Juan Ramirez <juanc.rampar@gmail.com>
2023-08-22 18:15:44 -0500

    Edit date notification

--- .local/bin/showdatenotification.sh
+++ .local/bin/showdatenotification.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 dunstify -i "schedule" "$(date +"%A")" "$(date +"%Y/%m/%d %H:%M")"
+#paplay ~/.config/dunst/audios/mixkit-fast-wall-clock-ticking-1063.wav
+paplay ~/.config/dunst/audios/time-passing-sound-effect-fast-clock-108403.mp3
